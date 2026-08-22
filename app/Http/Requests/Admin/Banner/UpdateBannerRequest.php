bf429bcbf2ddaacd481e244d5b005478765ff818
Nafis <nafisislam161@gmail.com>
2026-08-22 13:48:36 +0600

    banner

--- app/Http/Requests/Admin/Banner/UpdateBannerRequest.php
+++ app/Http/Requests/Admin/Banner/UpdateBannerRequest.php
@@ -15,7 +15,7 @@ class UpdateBannerRequest extends BaseFormRequest
             'type' => ['required', Rule::enum(BannerTypeEnum::class)],
             'short_description' => ['nullable', 'string', 'max:250'],
             'redirect_url' => ['nullable', 'string', 'max:2048'],
-            'banner_image' => ['sometimes', 'image', 'mimes:jpeg,png,jpg,gif,webp', 'max:2048'],
+            'banner_image' => ['sometimes', 'image', 'mimes:jpeg,png,jpg,gif,webp', 'max:5120'],
             'is_active' => ['boolean'],
         ];
     }
